e332081f47201d760d1ee8660b825f8aa552b503
gowtham kishore <gowthamkishore3@gmail.com>
2024-02-18 18:37:56 -0800

    Adding minium Sorted Array

---
+++ minimumSortedArray.js
@@ -0,0 +1,15 @@
+var findMin = function(nums) {
+    let left = 0, right = nums.length-1;
+
+
+    while(left < right){
+        var mid = Math.floor((left + right)/2)
+        if (nums[mid] > nums[right]) left = mid + 1
+        else right = mid
+    }
+    return nums[left]
+
+};
+
+
+console.log(findMin([3,4,5,1,2]))
